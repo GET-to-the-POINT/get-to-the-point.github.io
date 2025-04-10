9a0da1c5f1158559bb8d82923158155e7fa38db6
XIYO <bunny@xiyo.dev>
2025-04-10 14:54:30 +0900

    :sparkles:

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,6 @@
-준비중
+<section class="flex h-screen items-center justify-center bg-[#E2E9F3]">
+	<div class="text-center">
+		<h1 class="text-4xl font-bold">Contact Us</h1>
+		<p class="mt-4 text-lg">준비중입니다.</p>
+	</div>
+</section>
