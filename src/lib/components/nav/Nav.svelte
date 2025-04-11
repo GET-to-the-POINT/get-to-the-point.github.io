025f4b0ffb4a10fc8ed4115a2b2fbbc314c00592
XIYO <bunny@xiyo.dev>
2025-04-11 16:14:29 +0900

    :lipstick: 내비게이션을 좀 더 모던하게 변경

--- src/lib/components/nav/Nav.svelte
+++ src/lib/components/nav/Nav.svelte
@@ -1,26 +1,33 @@
 <script>
 	import { page } from '$app/state';
 
-	import { HouseIcon } from '@lucide/svelte';
+	import { ContactIcon, HouseIcon, ScrollText, ScrollTextIcon } from '@lucide/svelte';
 </script>
 
-<header class="p-4 flex items-center justify-between sticky left-0 right-0 top-0 z-50 border-b py-4 bg-background/80 backdrop-blur-sm shadow-sm">
-	<h1>
-		<a href="/">
+<header class="[&_a]:p-4 [&_a]:block [&_a]:hover:shadow-2xl border-t flex justify-between sticky left-0 right-0 top-0 z-50 border-b bg-background/80 backdrop-blur-sm shadow-sm">
+	<h1 class="flex-1">
+		<a href="/" class="content-center">
 			{page.data.title ?? 'GET to the POINT'}
 		</a>
 	</h1>
 
 	<nav>
-		<ul class="flex items-center gap-4">
+		<ul class="flex h-full [&>*]:border-l">
 			<li>
-				<a href="/" variant="ghost">
+				<a href="/">
 					<HouseIcon class="inline-block mr-2 h-4 w-4" />
 					Home
 				</a>
 			</li>
 			<li>
-				<a href="/contact" variant="ghost">Contact</a>
+				<a href="/contact">
+					<ContactIcon class="inline-block mr-2 h-4 w-4" />
+					Contact</a>
+			</li>
+			<li>
+				<a href="/backstage">
+					<ScrollTextIcon class="inline-block mr-2 h-4 w-4" />
+					backstage</a>
 			</li>
 		</ul>
 	</nav>
