00b77ee0e7fe3bbfa7e23a9a399ac7d00430e498
XIYO <bunny@xiyo.dev>
2025-04-14 00:54:51 +0900

    :recycle: 선

--- src/lib/components/timeline/TimelineItem.svelte
+++ src/lib/components/timeline/TimelineItem.svelte
@@ -2,8 +2,7 @@
 	import { Card } from '$lib/components/ui/card';
 	import { getLocale } from '$lib/paraglide/runtime.js';
 	import { datetime } from '$lib/paraglide/registry.js';
-	import { Calendar1 } from '@lucide/svelte';
-	import { CalendarClock } from '@lucide/svelte';
+	import { Calendar1, CalendarClock } from '@lucide/svelte';
 
 	const { meta, gitLog, value } = $props();
 
@@ -16,42 +15,31 @@
 	};
 </script>
 
-<div>
-	<div class="mb-2">
-		<Card class="overflow-hidden [&>*]:px-4 [&>*:first-child]:pt-4 [&>*:last-child]:pb-4">
-			<div class="flex justify-between">
-				<h1 class="mb-2 font-extrabold">
-					{meta.title}
-				</h1>
+<div class="[&>div:first-child]:mb-2">
+	<div>
+		<Card class="[&_h1]:font-extrabold overflow-hidden [&>*]:px-4 [&>*:first-child]:pt-4 [&>*:last-child]:pb-4">
+			<h1>
+				{meta.title}
+			</h1>
 
-				<div
-					class="
-						text-gray-700
-						before:mr-2
-						before:text-sm
-						before:content-['작성자']
-						[&>span]:inline-block
-						[&>span]:rounded-full
-						[&>span]:bg-gray-200
-						[&>span]:px-2 [&>span]:py-1 [&>span]:text-xs [&>span]:font-semibold"
-				>
+			<div
+				class="text-right text-gray-700 [&>span]:mr-2 [&>span]:text-sm [&>ul]:inline-block [&>ul>li]:inline-block [&>ul>li]:rounded-full [&>ul>li]:bg-gray-200 [&>ul>li]:px-2 [&>ul>li]:py-1 [&>ul>li]:text-xs [&>ul>li]:font-semibold"
+			>
+				<span>작성자</span>
+				<ul>
 					{#each gitLog as log}
-						<span>
+						<li>
 							{log.author}
-						</span>
+						</li>
 					{/each}
-				</div>
+				</ul>
 			</div>
 
-			<div>
-				{@html value}
-			</div>
+			{@html value}
 		</Card>
 	</div>
 
-	<div
-		class="text-end text-sm text-gray-700 [&>div]:mb-1 [&>div>*:first-child]:inline-block"
-	>
+	<div class="text-end text-sm text-gray-700 [&>div]:mb-1 [&>div>*:first-child]:inline-block">
 		<div>
 			<Calendar1 />
 			<span>최초 작성 : {datetime(getLocale(), gitLog.at(0).datetime, formattedOptions)}</span>
