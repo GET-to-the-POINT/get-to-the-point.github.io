09dc655472e5b3bf092ec84d45e4fad99d01749a
XIYO <bunny@xiyo.dev>
2025-04-17 10:19:56 +0900

    :art: 백스테이지 글 순서를 최근 순이 상단에 나오도록 변경

--- src/routes/backstage/+page.server.js
+++ src/routes/backstage/+page.server.js
@@ -4,9 +4,8 @@ import Category from '$lib/post/Category.js';
 export async function load({ url }) {
 	const pathname = '/posts' + url.pathname;
 	const category = await Category.getCategory(pathname)?.toSerialize();
-	const reversedPosts = category.allPosts.slice().reverse();
 
 	return {
-		posts: reversedPosts
+		posts: category.allPosts,
 	};
 }
